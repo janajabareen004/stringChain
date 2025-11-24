4038fe831f0b79d955cc833d073e7d693620119f
janajabareen004 <jana.jabareen04@gmail.com>
2025-11-24 16:29:41 +0200

    end22

--- stringCahin/src/test/java/stringCahin/stringCahin/TestSystem.java
+++ stringCahin/src/test/java/stringCahin/stringCahin/TestSystem.java
@@ -1,4 +1,19 @@
+package stringCahin.stringCahin;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class TestSystem {
 
+    @Test
+    public void testFullSystem() {
+        E e = new E();
+        D d = new D(e);
+        C c = new C(d);
+        B b = new B(c);
+        A a = new A(b);
+
+        String result = a.process("");
+
+        assertEquals("HELLO", result);
+    }
 }
